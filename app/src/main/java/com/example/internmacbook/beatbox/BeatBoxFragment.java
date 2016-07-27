b1acb7fe2c2d1df80a7e713bba8d1a14e399e108
Intern MacBook3 <internmacbook@Tonic-2011-spare-pro.local>
2016-07-27 16:39:40 -0400

    post chapter 18, interface added

--- app/src/main/java/com/example/internmacbook/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/example/internmacbook/beatbox/BeatBoxFragment.java
@@ -9,13 +9,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.util.List;
+
 /**
  * Created by internmacbook on 7/27/16.
  */
 public class BeatBoxFragment extends Fragment {
 
+    private BeatBox mBeatBox;
+
     public static BeatBoxFragment newInstance(){
         return new BeatBoxFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        mBeatBox = new BeatBox(getActivity());
     }
 
     @Override
@@ -24,7 +35,7 @@ public class BeatBoxFragment extends Fragment {
 
         RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.fragment_beat_box_recycler_view);
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
-        recyclerView.setAdapter(new SoundAdapter());
+        recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
 
         return view;
     }
@@ -34,16 +45,26 @@ public class BeatBoxFragment extends Fragment {
 
     private class  SoundHolder extends RecyclerView.ViewHolder {
         private Button mButton;
-
+        private Sound mSound;
 
         public SoundHolder(LayoutInflater inflater, ViewGroup container){
             super(inflater.inflate(R.layout.list_item_sound, container, false));
             mButton = (Button)itemView.findViewById(R.id.list_item_sound_button);
         }
 
+        public void bindSound(Sound sound){
+            mSound = sound;
+            mButton.setText(mSound.getName());
+        }
     }
 
     private class SoundAdapter extends RecyclerView.Adapter<SoundHolder>{
+        private List<Sound> mSounds;
+
+        public SoundAdapter(List<Sound> sounds) {
+            mSounds = sounds;
+        }
+
         @Override
         public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater inflater = LayoutInflater.from(getActivity());
@@ -51,13 +72,14 @@ public class BeatBoxFragment extends Fragment {
         }
 
         @Override
-        public void onBindViewHolder(SoundHolder holder, int position) {
-
+        public void onBindViewHolder(SoundHolder soundHolder, int position) {
+            Sound sound = mSounds.get(position);
+            soundHolder.bindSound(sound);
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return mSounds.size();
         }
     }
 }
---
+++ app/src/main/java/com/example/internmacbook/beatbox/Sound.java
@@ -0,0 +1,34 @@
+package com.example.internmacbook.beatbox;
+
+/**
+
+ * Created by internmacbook on 7/27/16.
+ */
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath){
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length-1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public void setAssetPath(String assetPath) {
+        mAssetPath = assetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+}
